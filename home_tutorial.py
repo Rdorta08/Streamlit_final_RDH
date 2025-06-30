45fe969fd326ec66a507d59309fecdb8d256669c
José Manuel Pérez Sepúlveda <jomapesep@gmail.com>
2025-06-30 21:29:27 +0000

    Fixed HomeT

--- home_tutorial.py
+++ home_tutorial.py
@@ -1,21 +1,15 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+
 def main():
     st.title("Sistema de Recomendación para hacerte rico 💼")
     st.write("Este sistema te ayudará a comparar tu perfil con quienes ganan más de 50K y te dará recomendaciones.")
 
     if st.button("Comenzar recomendación"):
-        st.session_state['page'] = 'recomendacion'
-
-    # If page state is recomendacion, import and run that page's main()
-    if st.session_state.get('page') == 'recomendacion':
-        import recomendacion
-        recomendacion.main()
+        switch_page("recomendacion")  
 
 if __name__ == "__main__":
-    if 'page' not in st.session_state:
-        st.session_state['page'] = 'home'
     main()
 
 # https://docs.streamlit.io/library/get-started/multipage-apps
